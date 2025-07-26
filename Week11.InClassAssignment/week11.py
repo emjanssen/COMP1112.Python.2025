67acef8f0312a09a967e09cf9ddbaaa87f3829ef
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-07-25 22:22:42 -0400

    Create week11.py

---
+++ Week11.InClassAssignment/week11.py
@@ -0,0 +1,47 @@
+"""
+Name: Elizabeth Janssen
+Student ID: 200627709
+Course: COMP1112
+Assignment: Week 11 - In Class Assignment
+Date: 2025.07.25 
+Program Description: Prompts the user with a list of words provided via a Notepad file, and times how long it takes the user to type the words.
+"""
+
+# - - - Imports - - - #
+
+import subprocess
+import time
+import datetime 
+
+# - - - Function Declarations - - - #
+
+#Function one: wait 30 seconds
+def wait30Seconds():
+    time.sleep(30)
+
+def wait10Seconds():
+    time.sleep(10)
+
+#Function three: launches Notepad file, prints list of words
+
+def launchNotepadFile():
+
+    openNotepadFile = subprocess.Popen()
+    #try catch to ensure file has launched properly
+    return True
+
+#Function four: instruct user to print words
+
+def promptForUserInput():
+    userInput = []
+
+    return userInput
+#Function five: validate input
+
+def validateUserInput():
+
+    return True
+#Function six: close notepad
+#Function seven: return time user took to print words
+
+# - - - Function Calls - - - #

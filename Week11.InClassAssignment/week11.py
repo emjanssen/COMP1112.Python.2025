31a8ecb0c07cdf14c54334a04df9e3b1a23455e0
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-07-25 23:20:20 -0400

    times how long notepad is open

--- Week11.InClassAssignment/week11.py
+++ Week11.InClassAssignment/week11.py
@@ -4,44 +4,56 @@ Student ID: 200627709
 Course: COMP1112
 Assignment: Week 11 - In Class Assignment
 Date: 2025.07.25 
-Program Description: Prompts the user with a list of words provided via a Notepad file, and times how long it takes the user to type the words.
+Program Description: Prompts the user with a list of words provided via a Notepad file, and time how long it takes the user to type the words.
 """
 
 # - - - Imports - - - #
 
-import subprocess
 import time
-import datetime 
+import re
+import os
+import subprocess
 
 # - - - Function Declarations - - - #
 
-#Function one: wait 30 seconds
-def wait30Seconds():
-    time.sleep(30)
+#Function one: wait n number of seconds
+def waitInSeconds(timeToWait):
+    print(f"Waiting {timeToWait} seconds...")
+    time.sleep(timeToWait)
+    print(f"Done waiting {timeToWait} seconds.")
 
-def wait10Seconds():
-    time.sleep(10)
+#Function two: wait 30 seconds before starting program
+def initialWait():
 
-#Function three: launches Notepad file, prints list of words
+    timeToWait = 30
+    waitInSeconds(timeToWait)
 
-def launchNotepadFile():
+def provideInstructions():
 
-    openNotepadFile = subprocess.Popen()
-    #try catch to ensure file has launched properly
-    return True
+    timeToWait = 5
+    #buffer so user has time to read the instructions
+    print("In five seconds, a Notepad fill open, and it will contain a sentence.\nPlease type that sentence into Notepad exactly as it's written.\nYou will be time on how long it takes you to rewrite the sentence.")
+    waitInSeconds(timeToWait)
+    print("Opening Notepad file...")
 
-#Function four: instruct user to print words
+#Function three: open Notepad file
+def openNotepadFile():
 
-def promptForUserInput():
-    userInput = []
+    notepadFilePath = r"C:\Users\E.M. Janssen\Documents\GitHub\COMP1112.Python.2025\Week11.InClassAssignment\listOfWords.txt"
+    openedFile = subprocess.Popen(["notepad.exe", notepadFilePath])
+    return openedFile
 
-    return userInput
-#Function five: validate input
-
-def validateUserInput():
-
-    return True
-#Function six: close notepad
-#Function seven: return time user took to print words
+def timeHowLongFileIsOpen(openedFile):
+    startTime = time.time()
+    openedFile.wait()
+    endTime = time.time()
+    elapsedTime = endTime - startTime
+    print(f"You took {elapsedTime} seconds to type the sentence in Notepad.")
+    return elapsedTime
 
 # - - - Function Calls - - - #
+
+#initialWait() - this works; don't call it every time while in dev
+provideInstructions()
+openedFile = openNotepadFile()
+elapsedTime = timeHowLongFileIsOpen(openedFile)

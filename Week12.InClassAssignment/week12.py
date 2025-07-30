33f1bb30f28da16b69138d109074f3b193b35fcb
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-07-30 19:58:54 -0400

    Create week12.py

---
+++ Week12.InClassAssignment/week12.py
@@ -0,0 +1,8 @@
+"""
+Name: Elizabeth Janssen
+Student ID: 200627709
+Course: COMP1112
+Assignment: Week 12 - In Class Assignment
+Date: 2025.07.30 
+Program Description: Word Report from Spreadsheet """
+
